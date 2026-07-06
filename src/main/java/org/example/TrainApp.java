2504b7667bde8c7650d592646d8a9a614f6b7fc5
Yuvaraj MC <yuvarajmc151515@gmail.com>
2026-07-06 15:03:56 +0530

    feat(UC6):Map Bogie to Capacity (HashMap)

--- src/main/java/org/example/TrainApp.java
+++ src/main/java/org/example/TrainApp.java
@@ -1,21 +1,22 @@
 package org.example;
-//       uniqueness+fastaccess+insertionorder
-import java.util.LinkedHashSet;
-import java.util.Set;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TrainApp {
     public static void main(String[] args) {
+
         System.out.println("=== Train Consist Management App ===");
 
-        Set<String> trainFormation = new LinkedHashSet<>();
+        Map<String, Integer> bogieCapacity = new HashMap<>();
 
-        trainFormation.add("Engine");
-        trainFormation.add("Sleeper");
-        trainFormation.add("Cargo");
-        trainFormation.add("Guard");
-        trainFormation.add("Sleeper");  // duplicate
+        bogieCapacity.put("Sleeper", 72);
+        bogieCapacity.put("AC Chair", 78);
+        bogieCapacity.put("First Class", 24);
 
-        System.out.println("Train formation: " + trainFormation);
-        System.out.println("Total unique bogies: " + trainFormation.size());
+        System.out.println("Bogie capacity details:");
+        for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue() + " seats");
+        }
     }
 }
