39f44002086571cfc572f607e9e051d10fc3e9dc
Yuvaraj MC <yuvarajmc151515@gmail.com>
2026-07-07 15:38:23 +0530

    feat(UC17): Sort Bogie Names Using Arrays.sort()

--- src/main/java/org/example/TrainApp.java
+++ src/main/java/org/example/TrainApp.java
@@ -4,45 +4,30 @@ import java.util.Arrays;
 
 /**
  * =========================================================
- * MAIN CLASS – UseCase16TrainConsistMgmnt
+ * MAIN CLASS – UseCase17TrainConsistMgmnt
  * =========================================================
  *
- * Use Case 16: Sort Passenger Bogies by Capacity (Bubble Sort)
+ * Use Case 17: Sort Bogie Names Using Arrays.sort()
  *
  * Description:
- * This class sorts passenger bogie capacities using the
- * Bubble Sort algorithm manually, WITHOUT using
- * Arrays.sort() or Collections.sort().
+ * This class sorts bogie type names alphabetically using
+ * Java's built-in Arrays.sort() method (no manual logic).
  * =========================================================
  */
 public class TrainApp {
 
     public static void main(String[] args) {
         System.out.println("===============================================");
-        System.out.println(" UC16 - Bubble Sort (Capacities) ");
+        System.out.println(" UC17 - Sort Bogie Names (Arrays.sort) ");
         System.out.println("===============================================\n");
 
-        // Array of passenger bogie capacities (unsorted)
-        int[] capacities = {72, 24, 90, 56, 40};
+        // Array of bogie type names (unsorted)
+        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "General", "Cargo"};
 
-        System.out.println("Before sorting: " + Arrays.toString(capacities));
+        System.out.println(Arrays.toString(bogieNames));
 
-        int n = capacities.length;
-
-        // Bubble Sort: nested loops
-        for (int i = 0; i < n - 1; i++) {              // number of passes
-            for (int j = 0; j < n - 1 - i; j++) {      // compare adjacent
-                if (capacities[j] > capacities[j + 1]) {
-                    // swap using a temp variable
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
-
-        System.out.println("After sorting:  " + Arrays.toString(capacities));
-
-        System.out.println("\nUC16 bubble sort completed...");
+      String [] sorted =  bogieNames.clone() ;
+      Arrays.sort(sorted);
+        System.out.println(Arrays.toString(sorted));
     }
 }
